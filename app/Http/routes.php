04fbc88e103293be47ba728956cccdfc6a1f00e3
cbellom <cbellom@unal.edu.co>
2015-08-11 16:43:18 -0500

    json for processes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,10 @@ Route::get("service/{id}","ServiceController@show");
 Route::post("service","ServiceController@store");
 Route::put("service/{id}","ServiceController@update");
 Route::delete("service/{id}","ServiceController@destroy");
+
+Route::get("processes","ProcessesController@index");
+Route::get("processes/{id}","ProcessesController@show");
+Route::post("processes","ProcessesController@store");
+Route::put("processes/{id}","ProcessesController@update");
+Route::delete("processes/{id}","ProcessesController@destroy");
+Route::get("processes/data","ProcessesController@index");
